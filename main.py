c81a436bcb5cf549a1b69ba768431ac9450e07a7
lxm10 <lucasxmoura@gmail.com>
2022-11-13 23:37:22 -0300

    Mealy working/need testing

--- main.py
+++ main.py
@@ -24,19 +24,14 @@ def translate_expression(expression, variables):
             else:
                 aux += var_dict[i.lower()] + '=\'1\''
 
-        elif i=='|':
+        elif i == '|':
             aux += ' or '
             
-        elif i=='&':
+        elif i == '&':
             
             aux += ' and '
         else:
             aux += i
-
-
-    #for i in expression
-
-
 
     return aux
 
@@ -48,12 +43,16 @@ def generate(file2open, name_of_fsm):
         Port ( clk : in STD_LOGIC;
                rst : in STD_LOGIC;'''
     entity += '\n'
+
     entity_end = "end FSM;\n"
+
     behave_begin = "\narchitecture Behavioral of FSM is\n"
+
     statereg_begin = '''stateReg:process(clk,rst)
 begin
     if rst = '1' then
         acts <= '''
+
     statereg_end = '''elsif rising_edge(clk) then
         acts <= nxts;    
     end if;
@@ -86,20 +85,27 @@ end process;'''
                 except:
                     states[dict_child['id']] = "000000"
                 states_transitions[dict_child['id']] = []
-            elif 'endArrow' in dict_child['style'] or 'orthogonalEdgeStyle' in dict_child['style']:#é bom melhorar esses try except
+
+            elif 'endArrow' in dict_child['style'] or 'orthogonalEdgeStyle' in dict_child['style']:  # é bom melhorar esses try except
                 boolexp = '00000'
                 outexp = '00000'
+
                 try:
                     if '/' in dict_child['value']:
-                        boolexp,outexp = dict_child['value'].split('/')
+                        boolexp, outexp = dict_child['value'].split('/')
+
                     else:
                         boolexp = dict_child['value']
                         outexp = '00000'
+
                 except:
+
                     pass
+
                 try:
                     transitions[dict_child['id']] = (dict_child['source'], dict_child['target'], boolexp, outexp)
                 except:
+
                     transitions[dict_child['id']] = (dict_child['source'], dict_child['target'], '000000','000000')
 
             elif 'swimlane' in dict_child['style']:
@@ -113,13 +119,24 @@ end process;'''
                 if inout[dict_child['parent']][0] == 'Variables':
                     list_of_inputs.append(dict_child['value'].split('=')[1])
                     var_location = dict_child['parent']
+    mealy_out = {}
+    for i in list(inout.keys()):
+        if inout[i][0] == 'Variables_mealy':
+            for j in inout[i][1]:
+                aux = j.split('=')
+                mealy_out[aux[0]] = aux[1]+'<'
 
-    list_of_outputs = set(list_of_outputs)
-    list_of_inputs = set(list_of_inputs)
+    for i in range(len(list_of_outputs)):
+        if list_of_outputs[i] in list(mealy_out.keys()):
+            list_of_outputs[i] = mealy_out[list_of_outputs[i]]
+
+    list_of_outputs = list(set(list_of_outputs))
+    list_of_inputs = list(set(list_of_inputs))
+    list_of_outputs.sort()
+    list_of_inputs.sort()
 
     for key in transitions.keys():
         states_transitions[transitions[key][0]].append(key)
-
 
     # my_inverted_dict = dict(map(reversed, my_dict.items()))
 
@@ -130,7 +147,7 @@ end process;'''
     for i in list_of_outputs:
         entity += '               ' + i[:-1] + ': out STD_LOGIC;\n'
     entity = entity[:-2]
-    entity +=');\n'+ entity_end+behave_begin+'\n'
+    entity += ');\n' + entity_end+behave_begin+'\n'
     entity += 'type states is ('
 
     for key in states.keys():
@@ -150,7 +167,7 @@ end process;'''
     entity += process_head
 
     entity += "\ncase acts is\n"
-    print(transitions)
+
     for key in states.keys():
         entity += "   when " + states[key] + "=>\n"
         if len(states_transitions[key]) == 1:
@@ -183,6 +200,13 @@ end process;'''
     entity += 'end case;\n'
     entity += 'end process;\n'
 
+    mealy_state_outputs = {}  # stores expression and outputs related to a state
+    for i in transitions:
+        if transitions[i][0] not in mealy_state_outputs:
+            mealy_state_outputs[transitions[i][0]] = []
+        mealy_state_outputs[transitions[i][0]].append ((transitions[i][2], transitions[i][3]))
+
+
     process_head = "\noutputLogic:process(acts,"
     for i in list_of_inputs:
         process_head += i + ','
@@ -196,25 +220,72 @@ end process;'''
         for i in inout.keys():
             if inout[i][0] == states[key]:
                 for output in inout[i][1]:
-                    entity+='       ' + output+';\n'
+                    entity += '       ' + output+';\n'
 
+        size = len(list(states_transitions[key]))
+        if size == 1:
+            aux = []
+            for i in mealy_state_outputs[key]:
+                aux = i[1].split(',')
+                for j in aux:
+                    if j.isupper():
+                        entity += "       "+mealy_out[j.lower()][:-1]+"<='1';\n"
+                    else:
+                        entity += "       "+mealy_out[j.lower()][:-1]+"<='0';\n"
+
+        elif size > 1:
+            cnt = 0
+            size = len(list(states_transitions[key]))
+            for transition in mealy_state_outputs[key]:
+                if transition[0] == 'else':
+                    aux2 = transition[1]
+                else:
+                    if cnt == 0:
+                        entity += '\n       if ' + translate_expression(transition[0],
+                                                                      inout[var_location][1]) + ' then \n'
+                        aux = transition[1].split(',')
+                        for j in aux:
+                            if j.isupper():
+                                entity += "             " + mealy_out[j.lower()][:-1] + "<='1';\n"
+                            else:
+                                entity += "             " + mealy_out[j.lower()][:-1] + "<='0';\n"
+                        cnt += 1
+
+                    elif cnt < size - 1 and cnt > 0:
+                        entity += '       elsif ' + translate_expression(transition[0],
+                                                                         inout[var_location][1]) + ' then\n'
+                        aux = transition[1].split(',')
+                        for j in aux:
+                            if j.isupper():
+                                entity += "             " + mealy_out[j.lower()][:-1] + "<='1';\n"
+                            else:
+                                entity += "             " + mealy_out[j.lower()][:-1] + "<='0';\n"
+                        cnt += 1
+            entity += '       else\n'
+
+            aux = aux2.split(',')
+            for j in aux:
+                if j.isupper():
+                    entity += "             " + mealy_out[j.lower()][:-1] + "<='1';\n"
+                else:
+                    entity += "             " + mealy_out[j.lower()][:-1] + "<='0';\n"
+            entity += '       end if;\n'
     entity += "   when others=>\n"
-    for i in inout[list(inout.keys())[1]][1]:#possivel fonte de erro
-
-        entity += "       "+i.split("<=")[0]+"<='0';\n"
+    for i in list_of_outputs:
+        entity += "       "+i[:-1]+"<='0';\n"
 
     entity += 'end case;\n'
     entity += 'end process;\n'
-    entity+='end Behavioral;'
+    entity += 'end Behavioral;'
     print(entity)
     file = open(name+'.vhd', 'w')
-    print()
+
     file.write(entity)
     file.close()
 
 
-#generate(str(sys.argv[1]), str(sys.argv[2]))
-#print("File ready! Please check the reset state as it is \"randomly\" chosen.")
+# generate(str(sys.argv[1]), str(sys.argv[2]))
+# print("File ready! Please check the reset state as it is \"randomly\" chosen.")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-     generate('teste.xml', 'fsm')
+    generate('teste.xml', 'fsm')
